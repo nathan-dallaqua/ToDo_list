2a0db4a41a6f0fa1fa7054b564b318f557adff63
Nathan Dallaqua <npdallaqua@gmail.com>
2023-02-12 09:12:56 -0300

    todo_list
    Feito no Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  public tarefa = "";
+  public items = [''];
+
+  addTarefa() {
+    this.items.push(this.tarefa);
+  }
+
+  removeTarefa(item: string) {
+    this.items.splice( this.items.indexOf(item),1);
+  }
   title = 'ToDo_list';
 }
+
+
